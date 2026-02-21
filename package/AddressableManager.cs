f6a252978ef6edb30df9b76afbf47f4c4d521161
GregKingDev <Greg.King.dev@gmail.com>
2026-02-21 11:51:25 -0400

    Switching to manager

---
+++ package/AddressableManager.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
+namespace GrygTools.AddressableUtils
+{
+	public class AddressableManager : SingletonBase<AddressableManager>, ISingleton
+	{
+		private readonly Dictionary<AssetReference, string> referenceToLocation = new();
+		private readonly Dictionary<string, AsyncOperationHandle> locationToHandles = new();
+
+		public void Init() {}
+
+		public bool TryLoadAssetReference<T>(AssetReference assetReference, out T tObject) where T : class
+		{
+			tObject = LoadAssetReference<T>(assetReference);
+			return tObject != null;
+		}
+		
+		public async Task<(bool, T)> TryLoadAssetReferenceAsync<T>(AssetReference assetReference) where T : class
+		{
+			T tObject = await LoadAssetReferenceAsync<T>(assetReference);
+			return (tObject != null, tObject);
+		}
+
+		public bool TryGetIfLoaded<T>(AssetReference assetReference, out T asset) where T : class
+		{
+			string location = GetLocationFromAssetReferenceAsync(assetReference).Result;
+			if (locationToHandles.TryGetValue(location, out AsyncOperationHandle handle))
+			{
+				asset = handle.Result as T;
+				return asset != null;
+			}
+			asset = null;
+			return false;
+		}
+		
+		public async Task<T> LoadAssetReferenceAsync<T>(AssetReference assetReference) where T : class
+		{
+			string location = await GetLocationFromAssetReferenceAsync(assetReference);
+
+			return await LoadAssetFromLocationAsync<T>(location);
+		}
+
+		public async Task<T> LoadAssetFromLocationAsync<T>(string location) where T : class
+		{
+			if (!locationToHandles.TryGetValue(location, out AsyncOperationHandle handle))
+			{
+				handle = Addressables.LoadAssetAsync<T>(location);
+				locationToHandles.Add(location, handle);
+			}
+			
+			if (handle.Status != AsyncOperationStatus.Succeeded)
+			{
+				await handle.Task;
+			}
+			
+			if (handle.Status == AsyncOperationStatus.Succeeded)
+			{
+				if (handle.Result is T thing)
+				{
+					return thing;
+				}
+				Debug.LogError($"Asset at location {location} is not type {typeof(T)}");
+				return null;
+			}
+			Debug.LogError($"Unable to load asset reference with location {location}");
+			return null;
+		}
+		
+		public T LoadAssetReference<T>(AssetReference assetReference) where T : class
+		{
+			return LoadAssetReferenceAsync<T>(assetReference).Result;
+		}
+
+		public T LoadAssetFromLocation<T>(string location) where T : class
+		{
+			return LoadAssetFromLocationAsync<T>(location).Result;
+		}
+
+		public void ReleaseAssetReference(AssetReference assetReference)
+		{
+			string location = GetLocationFromAssetReferenceAsync(assetReference).Result;
+			if (locationToHandles.TryGetValue(location, out AsyncOperationHandle handle))
+			{
+				Addressables.Release(handle);
+			}
+		}
+
+		private async Task<string> GetLocationFromAssetReferenceAsync(AssetReference assetReference)
+		{
+			if(referenceToLocation.TryGetValue(assetReference, out string location))
+			{
+				return location;
+			}
+			var handle = Addressables.LoadResourceLocationsAsync(assetReference);
+			await handle.Task;
+			if (handle.Status == AsyncOperationStatus.Failed || handle.Result.Count == 0)
+			{
+				Debug.LogError($"Failed to load address for AssetReference with guid {assetReference.AssetGUID}");
+				return null; 
+			}
+			referenceToLocation.Add(assetReference, handle.Result[0].PrimaryKey);
+			return handle.Result[0].PrimaryKey;
+		}
+		
+		public async Task<IEnumerable<object>> LoadLabel(string label)
+		{
+			var handle = Addressables.LoadResourceLocationsAsync(label);
+
+			await handle.Task;
+
+			if (handle.Status == AsyncOperationStatus.Succeeded)
+			{
+				List<Task<object>> tasks = new();
+				
+				foreach (IResourceLocation location in handle.Result)
+				{
+					tasks.Add(LoadAssetFromLocationAsync<object>(location.PrimaryKey));
+				}
+				await Task.WhenAll(tasks);
+				return tasks.Select(a => a.Result);
+			}
+			Debug.LogError($"Error loading label: {label}");
+			return null;
+		}
+	}
+}
